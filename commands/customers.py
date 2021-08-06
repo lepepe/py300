0d5dacb13fdfb8f7e2d461c47ead60c10d75ea86
Jose Perez <jose.perez@vertilux.com>
2021-08-06 13:42:15 -0400

    setup rich library for farmatting outputs

--- commands/customers.py
+++ commands/customers.py
@@ -1,49 +1,95 @@
 import click
 import pyodbc
 import pandas as pd
-from prettytable import PrettyTable
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+from rich.layout import Layout
 import config
+from config import currency
+
+console = Console()
+layout = Layout()
 
 con = pyodbc.connect('DRIVER={FreeTDS};SERVER='+config.SQL_SERVER+';DATABASE='+config.DB_NAME+';PORT='+config.SQL_PORT+';UID='+config.SQL_USER+';PWD='+ config.SQL_PASSWD)
 cursor = con.cursor()
 
 def customers(results):
-    t = PrettyTable(['Customer Number', 'Customer Name', 'On Hold', 'Country', 'City'])
-    t.align = "l"
-    for r in results:
-        t.add_row(
-            [
-                r.IDCUST.rstrip(),
-                r.NAMECUST.rstrip(),
-                r.SWHOLD,
-                r.CODECTRY.rstrip(),
-                r.NAMECITY.rstrip()
-            ]
-        )
-    return t
+    table = Table(title="Customer(s)")
+    table.add_column("Customer Number", justify="left", no_wrap=True)
+    table.add_column("Custoemer Name", justify="left", no_wrap=True)
+    table.add_column("On Hold", justify="left", no_wrap=True, style="magenta")
+    table.add_column("Country", justify="left", no_wrap=True)
+    table.add_column("City", justify="left", no_wrap=True)
 
-def credit_available(limit, balance):
-    a = limit-balance
-    if a > 0:
-        click.secho(f"Outstanding Balance: {balance}", fg="green", bold=True),
-        click.secho(f"Available: {a}", fg="green", bold=True),
-    else:
-        click.secho(f"Available: {a}", fg="red", bold=True),
+    for r in results:
+        table.add_row(r.IDCUST.rstrip(), r.NAMECUST.rstrip(), str(r.SWHOLD), r.CODECTRY.rstrip(), r.NAMECITY.rstrip())
+    return table
+
+def credit_available(results):
+    for r in results:
+        limit = r.AMTCRLIMT
+        balance = r.AMTBALDUET
+        terms = r.CODETERM
+        a = limit-balance
+
+        if a > 0:
+            panel = (
+                f"Credit limit: [green]{currency(limit)}[/green]\n"
+                f"Outstarnding Balance: [green]{currency(balance)}[/green]\n"
+                f"Available: [green]{currency(a)}[/green]\n"
+                f"Terms: [blue]{terms}[/blue]"
+            )
+        else:
+            panel = (
+                f"Credit limit: [red]{currency(limit)}[/red]\n"
+                f"Outstarnding Balance: [red]{currency(balance)}[/red]\n"
+                f"Available: [red]{currency(a)}[/red]\n"
+                f"Terms: [blue]{terms}[/blue]"
+            )
+
+    return panel
 
 @click.command()
 @click.option("-a", "--account", required=False, help="Customer account number")
-def cli(account):
+@click.option("-f", "--find", required=False, help="Find customer by account number or name")
+def cli(account, find):
     """ Table: Customers (ARCUS) """
 
     if account:
         results = cursor.execute("SELECT * FROM ARCUS WHERE IDCUST = ?", account).fetchall()
-        print(customers(results))
+        customers(results)
+        credit_available(results)
 
-        for r in results:
-            click.secho(f"Credit limit: {r.AMTCRLIMT}", fg="blue", bold=True),
-            credit_available(r.AMTCRLIMT, r.AMTBALDUET)
-            click.secho(f"Terms: {r.CODETERM}", fg="blue", bold=True)
+        layout.split_column(
+            Layout(name="header", size=3),
+            Layout(name="upper", size=8),
+            Layout(name="lower", size=15)
+        )
+
+        # Divide the lower layout in two parts
+        layout["lower"].split_row(
+            Layout(name="credit"),
+            Layout(name="right"),
+        )
+
+        # Rendered data into the layouts
+        layout["header"].split(
+            Layout(Panel(f"Customer: [blue]{account}[/blue]"))
+        )
+        layout["credit"].update(
+            Panel(credit_available(results), title="Credit Status")
+        )
+        layout["upper"].update(
+            customers(results)
+        )
+        console.print(layout)
+
+    elif find:
+        results = cursor.execute(f"SELECT * FROM ARCUS WHERE IDCUST LIKE '%{find}%' OR NAMECUST LIKE '%{find}%'").fetchall()
+        console.print(f"Total records found: [blue]{len(results)}[/blue]")
+        console.print(customers(results))
+
     else:
-        print("All Customers")
-        results = cursor.execute(""" SELECT * FROM ARCUS """).fetchall()
-        print(customers(results))
+        results = cursor.execute("SELECT COUNT(*) FROM ARCUS")
+        console.print(f"Total customers: [blue]{results.fetchone()[0]}[/blue]")
--- config.py
+++ config.py
@@ -7,3 +7,7 @@ DB_NAME = os.environ.get("db_name")
 SQL_USER = os.environ.get("sql_user")
 SQL_PASSWD = os.environ.get("sql_passwd")
 SQL_PORT = os.environ.get("sql_port")
+
+def currency(value):
+    formatted_float = "${:,.2f}".format(value)
+    return formatted_float
